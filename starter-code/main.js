9a694a42670e66b13a4a3dd06aef9629bbdf2096
traciwilliams <ug3000@gmail.com>
2016-09-13 17:36:20 -0500

    Add files via upload

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,17 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+
+if (cardOne == cardTwo) {
+		window.alert ("That's correct");
+}	
+
+	else if (cardThree == cardFour) {
+		window.alert ("That's correct");
+}
+
+	else {
+		window.alert ("That's incorrect. Please try again.")
+	}
